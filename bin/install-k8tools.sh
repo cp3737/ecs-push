5af343421b74f4a3421383eba348feba7b533c83
Sonya Mollinger <sonya.mollinger@gmail.com>
2017-11-09 20:53:58 -0800

    fix 4

--- bin/install-k8tools.sh
+++ bin/install-k8tools.sh
@@ -1,20 +1,17 @@
 # Download, Install and Configure ecs-cli
 #Some txt
-
 #set the environment variables
 source ./bin/env.sh
 source ./bin/env-kube.sh
-
 rm -rf ~/.ecs/config
-
 #Download the correct cli for your os
 if [ "$(uname)" == "Darwin" ]; then
   # Get the Mac binaries
   curl -L https://github.com/kubernetes-incubator/kompose/releases/download/v0.7.0/kompose-darwin-amd64 -o kompose
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
  sudo curl -L https://github.com/kubernetes-incubator/kompose/releases/download/v0.7.0/kompose-linux-amd64 -o kompose
-
 fi
+
 sudo mv ./kompose /usr/local/bin/kompose
 sudo chmod +x /usr/local/bin/kompose
 
